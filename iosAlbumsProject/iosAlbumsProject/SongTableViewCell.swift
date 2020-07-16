2c0f56583e97c38f844a3f66a58a71143fcfe433
Brys1Saclaus2 <brys1saclaus2@gmail.com>
2020-07-16 13:01:22 -0700

    storyboardSetup

---
+++ iosAlbumsProject/iosAlbumsProject/SongTableViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  SongTableViewCell.swift
+//  iosAlbumsProject
+//
+//  Created by BrysonSaclausa on 7/16/20.
+//  Copyright © 2020 Lambda. All rights reserved.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+
+    
+    @IBAction func songNameTextField(_ sender: Any) {
+    }
+    @IBAction func songDurationTextField(_ sender: Any) {
+    }
+    @IBAction func addSongButton(_ sender: Any) {
+    }
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBAction func addSongButtonTapped(_ sender: Any) {
+    }
+    
+
+}
